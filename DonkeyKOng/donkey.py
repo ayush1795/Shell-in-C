6abeb85318d0039ab319cdb2aa38a155b8804a89
ayushjoshi <ayush.joshi@research.iiit.ac.in>
2016-01-18 23:00:42 +0530

    remove DonkeyKOng

--- DonkeyKOng/donkey.py
+++
@@ -1,7 +0,0 @@
-#Inheritence
-from pplayer import Player
-class Donkey(Player):
-	def __init__(self):
-		pass
-	def set_pos(self,x,y):
-		self.arr[x][y] = 'D'
--- DonkeyKOng/queen.py
+++
@@ -1,8 +0,0 @@
-#Inheritence
-from pplayer import Player
-class Queen(Player):
-	def __init__(self):
-		pass
-	def set_pos(self,x,y):
-		self.arr[x][y] = 'Q'
-
